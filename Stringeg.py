4569970bd0725b56812970b35f0e0a8c00063536
ICT1 <thubtenugyennorbu@gmail.com>
2026-03-18 09:58:29 +0600

    Using String as an example

---
+++ Stringeg.py
@@ -0,0 +1,10 @@
+print("Hello World")
+multiplelines = '''Python is a high-level, interpreted programming language known for its clear,concise syntax and high readability,
+making it ideal for beginners.It is highly versatile, supporting multiple programming paradigms including object-oriented and functional, for diverse applications.'''
+print(multiplelines)
+a = 5
+b = 6
+c = a+b
+print (type(c)) #Data type of c is integer
+c = str(c) #Type Casting
+print (type(c)) #Data Type of c is now str/string
