bffae558f7c0b5a6d3abe4a1305ffd1abe68367f
ICT1 <thubtenugyennorbu@gmail.com>
2026-03-20 15:09:12 +0600

    Learning arithematic

---
+++ Arithmetic.py
@@ -0,0 +1,14 @@
+a = 10
+b = 5 
+additon_result = a + b  #10+5 = 15
+subtraction_result = a - b   #10-5 = 5
+multiplication_result = a * b #10 * 5 = 50
+division_result = a / b  #10 / 5=  2(division always returns a float)
+exponential_result = a ** b  # 10 ** 5 = 100000
+modulus_result = a % b  # 10 % 5 = 0 (remainder of division)
+print(additon_result)
+print(subtraction_result)
+print(multiplication_result)
+print(division_result )
+print(exponential_result)
+print(modulus_result)
