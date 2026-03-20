44edd0e873f91489669d0d6c2fef8ae3d3188f22
ICT1 <thubtenugyennorbu@gmail.com>
2026-03-20 15:39:46 +0600

    area of a rectangle

---
+++ area of  a rectangle.py
@@ -0,0 +1,5 @@
+L = float(input("Enter the length of the rectangle:"))
+B = float(input("Enter the breadth of the rectangle:"))
+area = L * B
+print("The area of the rectangle is:", area)
+print(type(L))
