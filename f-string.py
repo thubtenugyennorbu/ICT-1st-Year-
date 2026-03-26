74acc17d14d0006391522f1ebf8271b9bb436e58
ICT1 <thubtenugyennorbu@gmail.com>
2026-03-26 16:10:12 +0600

    learning f-string function

---
+++ f-string.py
@@ -0,0 +1,6 @@
+topic = "formating strings"
+print(f"We are currently learning {topic}") 
+#We are currently learning formatting strings
+
+print(f"There are 12 girls and 16 boys in our class. So in total we have {12 +16} students")
+#Output: There are 12 girls and 16 boys in our class. So in total we 28 students
